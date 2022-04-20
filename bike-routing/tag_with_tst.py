39d0b464b028d199f8c36ae9cabcda16a6c3628b
Vincent Wilson <vince@classyh.at>
2022-04-19 21:37:00 -0400

    Added bike routing engine

---
+++ bike-routing/tag_with_tst.py
@@ -0,0 +1,58 @@
+import psycopg2
+import requests
+import json
+from dotenv import load_dotenv
+# import os
+
+# load_dotenv()  # take environment variables from .env.
+
+def get_tst_data():
+    lines = []
+    try:
+        connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+        cursor = connection.cursor()
+        map_features = "select data, ST_AsGeoJSON(geog_line), type from features where type like 'tst%'"
+
+        cursor.execute(map_features)
+        lines = cursor.fetchall()
+
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    return lines
+        
+
+
+
+def match_line(coords, route):
+    parsed_coords = ";".join([",".join([str(c) for c in coord]) for coord in coords])
+    matches = requests.get(
+        f"http://router.project-osrm.org/match/v1/bike/{parsed_coords}?steps=false&geometries=geojson&overview=false&annotations=true"
+    )
+
+    try:
+        return matches.json()["matchings"][0]["legs"][0]["annotation"]["nodes"]
+    except KeyError:
+        print("Key Error")
+        print(matches.json())
+        print(route)
+        return []
+        
+
+data = {}        
+lines = get_tst_data()
+for route in lines:
+    if route[2] not in data:
+        data[route[2]] = []
+    points = match_line(json.loads(route[1])["coordinates"], route)
+    data[route[2]].append(points)
+
+with open('tst_tagged_nodes.json', 'w') as outfile:
+    json.dump(data, outfile)
